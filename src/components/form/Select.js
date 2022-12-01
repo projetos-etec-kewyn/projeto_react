473220965c34855e9f71a33f1a0044f3db2ee4d4
Kewyn <kewyntorres10@gmail.com>
2022-11-30 23:51:40 -0300

    React

--- src/components/form/Select.js
+++ src/components/form/Select.js
@@ -9,6 +9,11 @@ function Select({text,name,options,handleOnChange,value}){
                 id={name}
             >
                 <option>Selecione uma opção</option>
+                {options.map((option) => (
+                    <option value={option.id} key={option.id}>
+                        {option.name}
+                    </option>
+                ))}
             </select>
         </div>
     )
